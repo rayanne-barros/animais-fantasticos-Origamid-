85e456fe2c71e0fcb832004a9543129149867d9e
Rayanne Barros <rayanneb126@gmail.com>
2022-01-25 09:44:07 -0300

    Scroll Suave

--- script.js
+++ script.js
@@ -28,12 +28,9 @@ function initAccordion() {
     accordionList[0].classList.add('ativo');
     accordionList[0].nextElementSibling.classList.add('ativo');
 
-
-
     function activeAccordion() {
       this.classList.toggle('ativo');
       this.nextElementSibling.classList.toggle('ativo');
-
     }
 
     accordionList.forEach((item) => {
@@ -43,6 +40,33 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"');
+
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+
+initScrollSuave();
 
 
 
