00a09a46ddb8474760ab223130755cbf6b3ad830
Rayanne Barros <rayanneb126@gmail.com>
2022-02-13 01:51:18 -0300

    Dropdown no menu (Sobre)

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -11,4 +12,5 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
 
